8cca74c25af7e5006a5f5a1fb1c249d8f8b7ba56
iMokhles <mokhleshussien@aol.com>
2017-04-14 23:56:14 +0200

    Command to generate random Application Id ( php artisan gen:appid ) and has two options --show : Display the id instead of modifying files & --force : Force the operation to run when in production

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,8 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use App\Console\Commands\GenerateApplicationId;
+
 
 class Kernel extends ConsoleKernel
 {
@@ -13,7 +15,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        //
+        GenerateApplicationId::class,
     ];
 
     /**
--- config/imbaas_ids.php
+++ config/imbaas_ids.php
@@ -10,16 +10,20 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | Application Id
+    | Application Id-length-headerKey
     |--------------------------------------------------------------------------
     |
     | Your application id which will be sent from your client
-    | to authorize the that client
+    | to authorize that client
     |
+    | Your application id length is your application id length
+    |
+    | Your application id header key is your application id
+    | personal header's key will be sent from your client
     |
     */
 
-    'applicationId' => env('API_APPLICATION_ID', 'SomeRandomString'),
-    'applicationIdLength' => env('API_APPLICATION_ID_LENGTH', 16),
+    'applicationId' => env('API_APPLICATION_ID', 'TXEIbxMb90zpGIOBkar2q8MnRedYXzE0'),
+    'applicationIdLength' => env('API_APPLICATION_ID_LENGTH', 32),
     'applicationIdHeaderKey' => env('API_APPLICATION_ID_HEADER_KEY', 'X_IMBaaS_Application_Id'),
 ];
